f9378230412a78f646f22aeee6ec410cdc18f94a
Andreas Neugebauer <pneugebala@gmail.com>
2019-06-20 21:55:45 +0200

    feat: update

--- src/main/kotlin/com/auritylab/spring/gcp/tasks/core/executor/TaskExecutor.kt
+++ src/main/kotlin/com/auritylab/spring/gcp/tasks/core/executor/TaskExecutor.kt
@@ -1,12 +1,14 @@
 package com.auritylab.spring.gcp.tasks.core.executor
 
 import com.auritylab.spring.gcp.tasks.configurations.SpringGcpTasksConfigurationProperties
+import com.auritylab.spring.gcp.tasks.remote.TaskCredentialsService
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class TaskExecutor(
-        private val properties: SpringGcpTasksConfigurationProperties
+        private val properties: SpringGcpTasksConfigurationProperties,
+        private val taskCredentials: TaskCredentialsService
 ) {
     fun execute(queue: String, payload: String): UUID {
         val uuid = UUID.randomUUID()
--- src/main/kotlin/com/auritylab/spring/gcp/tasks/remote/TaskCredentialsService.kt
+++ src/main/kotlin/com/auritylab/spring/gcp/tasks/remote/TaskCredentialsService.kt
@@ -1,14 +1,24 @@
 package com.auritylab.spring.gcp.tasks.remote
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.HttpRequestInitializer
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.gax.core.CredentialsProvider
+import com.google.api.services.cloudtasks.v2.CloudTasks
+import com.google.api.services.cloudtasks.v2.CloudTasksRequestInitializer
 import org.springframework.stereotype.Service
 
 @Service
 class TaskCredentialsService(
         private val credentialsProvider: CredentialsProvider
 ) {
-    val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
-    val jsonFactory = JacksonFactory.getDefaultInstance()
+    private val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
+    private val jsonFactory = JacksonFactory.getDefaultInstance()
+
+    private val credentials = credentialsProvider.credentials
+
+    val cloudTasks = CloudTasks.Builder(
+            httpTransport,
+            jsonFactory,
+            HttpRequestInitializer {})
 }
