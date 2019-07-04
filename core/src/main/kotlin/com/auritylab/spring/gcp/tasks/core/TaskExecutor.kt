e15eae465db463f984f98bb70c55539077484e02
Andreas Neugebauer <pneugebala@gmail.com>
2019-07-04 19:17:03 +0200

    feat: switch to cloud tasks library

--- core/src/main/kotlin/com/auritylab/spring/gcp/tasks/core/TaskExecutor.kt
+++ core/src/main/kotlin/com/auritylab/spring/gcp/tasks/core/TaskExecutor.kt
@@ -2,7 +2,10 @@ package com.auritylab.spring.gcp.tasks.core
 
 import com.auritylab.spring.gcp.tasks.api.ITaskWorker
 import com.google.cloud.tasks.v2beta3.CloudTasksClient
+import com.google.cloud.tasks.v2beta3.HttpMethod
+import com.google.cloud.tasks.v2beta3.HttpRequest
 import com.google.cloud.tasks.v2beta3.Task
+import com.google.protobuf.ByteString
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -16,29 +19,24 @@ class TaskExecutor {
     fun execute(worker: ITaskWorker<*>, payload: String): UUID {
         val uuid = UUID.randomUUID()
         val queue = worker.getQueue().toString()
-        val base64payload = Base64.getEncoder().encodeToString(payload.toByteArray())
+        //val base64payload = Base64.getEncoder().encodeToString(payload.toByteArray())
 
         CloudTasksClient.create().use {
             val task = Task.newBuilder()
-        }
-
-        /*val requestBody = CreateTaskRequest().apply {
-            task = Task()
                 .setName("$queue/tasks/$uuid")
                 .setHttpRequest(
-                    HttpRequest()
-                        .setHttpMethod("POST") // ToDo: Maybe expose as property as well
-                        .setHeaders(mapOf(CLOUD_TASKS_SUB_ROUTE_HEADER to worker.getSubRoute()))
-                        .setHeaders(mapOf(CLOUD_TASKS_TASK_ID_HEADER to uuid.toString()))
+                    HttpRequest.newBuilder()
+                        .setHttpMethod(HttpMethod.POST) // ToDo: Maybe expose as property as well
+                        .putHeaders(CLOUD_TASKS_SUB_ROUTE_HEADER, worker.getSubRoute())
+                        .putHeaders(CLOUD_TASKS_TASK_ID_HEADER, uuid.toString())
                         .setUrl("${worker.getEndpoint()}${worker.getMainRoute()}")
-                        .setBody(base64payload)
+                        .setBody(ByteString.copyFromUtf8(payload))
+                        .build()
                 )
+                .build()
+
+            it.createTask(queue, task)
         }
-
-        val request = taskCredentials.getCloudTasks()
-                .projects().locations().queues().tasks().create(queue, requestBody)
-
-        val response = request.execute()*/
 
         return uuid
     }
