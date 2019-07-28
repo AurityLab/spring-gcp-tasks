2ade977d2c5ef7fd39e2cbd9ff2dc8b380c69746
Andreas Neugebauer <pneugebala@gmail.com>
2019-07-28 15:06:32 +0200

    chore: extend TaskQueue and TaskRequest tests

--- src/test/kotlin/com/auritylab/spring/gcp/tasks/api/utils/queue/TaskQueueTest.kt
+++ src/test/kotlin/com/auritylab/spring/gcp/tasks/api/utils/queue/TaskQueueTest.kt
@@ -3,7 +3,6 @@ package com.auritylab.spring.gcp.tasks.api.utils.queue
 import org.junit.jupiter.api.Test
 
 class TaskQueueTest {
-
     private fun checkQueueObject(projectId: String, locationId: String, queueId: String, queue: TaskQueue) =
         queue.build() == "projects/$projectId/locations/$locationId/queues/$queueId"
 
@@ -30,5 +29,39 @@ class TaskQueueTest {
         }
 
         assert(checkQueueObject(testProjectId, testLocationId, testQueueId, queue))
+    }
+
+    @Test
+    fun `Test builder of TaskQueue class with existing TaskQueue object as base`() {
+        val testProjectId = "some-project"
+        val testLocationId = "some-location"
+        val testQueueId = "some-queue"
+
+        val testProjectId2 = "another-project"
+        val testLocationId2 = "another-location"
+        val testQueueId2 = "another-queue"
+
+        val queue = TaskQueue {
+            setProjectId(testProjectId)
+            setLocationId(testLocationId)
+            setQueueId(testQueueId)
+        }
+
+        assert(checkQueueObject(testProjectId, testLocationId, testQueueId, queue))
+
+        assert(checkQueueObject(testProjectId2, testLocationId, testQueueId, TaskQueue {
+            fromTaskQueue(queue)
+            setProjectId(testProjectId2)
+        }))
+
+        assert(checkQueueObject(testProjectId, testLocationId2, testQueueId, TaskQueue {
+            fromTaskQueue(queue)
+            setLocationId(testLocationId2)
+        }))
+
+        assert(checkQueueObject(testProjectId, testLocationId, testQueueId2, TaskQueue {
+            fromTaskQueue(queue)
+            setQueueId(testQueueId2)
+        }))
     }
 }
--- src/test/kotlin/com/auritylab/spring/gcp/tasks/api/utils/request/TaskRequestTest.kt
+++ src/test/kotlin/com/auritylab/spring/gcp/tasks/api/utils/request/TaskRequestTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test
 import java.net.URL
 
 class TaskRequestTest {
-
     private fun checkRequestObject(endpoint: String, endpointRoute: String, workerRoute: String, request: TaskRequest) =
         request.buildRequestUrl() == URL(endpoint + endpointRoute) && request.workerRoute == workerRoute
 
@@ -31,5 +30,39 @@ class TaskRequestTest {
         }
 
         assert(checkRequestObject(testEndpoint, testEndpointRoute, testWorkerRoute, request))
+    }
+
+    @Test
+    fun `Test builder of TaskQueue class with existing TaskQueue object as base`() {
+        val testEndpoint = "http://localhost:3000"
+        val testEndpointRoute = "/taskhandler"
+        val testWorkerRoute = "/test"
+
+        val testEndpoint2 = "http://localhost:4000"
+        val testEndpointRoute2 = "/anotherhandler"
+        val testWorkerRoute2 = "/anotherroute"
+
+        val request = TaskRequest {
+            setEndpoint(testEndpoint)
+            setEndpointRoute(testEndpointRoute)
+            setWorkerRoute(testWorkerRoute)
+        }
+
+        assert(checkRequestObject(testEndpoint, testEndpointRoute, testWorkerRoute, request))
+
+        assert(checkRequestObject(testEndpoint2, testEndpointRoute, testWorkerRoute, TaskRequest {
+            fromTaskRequest(request)
+            setEndpoint(testEndpoint2)
+        }))
+
+        assert(checkRequestObject(testEndpoint, testEndpointRoute2, testWorkerRoute, TaskRequest {
+            fromTaskRequest(request)
+            setEndpointRoute(testEndpointRoute2)
+        }))
+
+        assert(checkRequestObject(testEndpoint, testEndpointRoute, testWorkerRoute2, TaskRequest {
+            fromTaskRequest(request)
+            setWorkerRoute(testWorkerRoute2)
+        }))
     }
 }
