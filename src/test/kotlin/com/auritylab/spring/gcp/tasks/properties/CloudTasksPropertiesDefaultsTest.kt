b831e2abe6b1eccabce5b5810ddb8ad147ad52c0
Andreas Neugebauer <pneugebala@gmail.com>
2019-07-30 16:02:41 +0200

    chore: add CloudTasksProperties tests

---
+++ src/test/kotlin/com/auritylab/spring/gcp/tasks/properties/CloudTasksPropertiesDefaultsTest.kt
@@ -0,0 +1,41 @@
+package com.auritylab.spring.gcp.tasks.properties
+
+import com.auritylab.spring.gcp.tasks.config.CloudTasksLibraryAutoConfiguration
+import com.auritylab.spring.gcp.tasks.config.EnableCloudTasks
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.gcp.core.GcpProjectIdProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@EnableCloudTasks
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(classes = [CloudTasksPropertiesDefaultsTest.TestConfiguration::class])
+@TestPropertySource("/test-defaults.properties")
+class CloudTasksPropertiesDefaultsTest {
+    @Configuration
+    class TestConfiguration : CloudTasksLibraryAutoConfiguration() {
+        @Bean
+        fun gcpProjectIdProvider(): GcpProjectIdProvider = GcpProjectIdProvider { "some-project-by-provider" }
+    }
+
+    @Test
+    fun `Test properties for default values`(
+        @Autowired properties: CloudTasksProperties
+    ) {
+        assert(properties.defaultProjectId == null)
+        assert(properties.defaultLocationId == null)
+        assert(properties.defaultQueueId == null)
+
+        assert(properties.defaultWorkerEndpoint == null)
+        assert(properties.defaultWorkerEndpointRoute == "/taskhandler")
+        assert(properties.defaultWorkerRoute == "")
+
+        assert(!properties.skipCloudTasks)
+        assert(!properties.skipTaskEndpoint)
+    }
+}
---
+++ src/test/kotlin/com/auritylab/spring/gcp/tasks/properties/CloudTasksPropertiesTest.kt
@@ -0,0 +1,41 @@
+package com.auritylab.spring.gcp.tasks.properties
+
+import com.auritylab.spring.gcp.tasks.config.CloudTasksLibraryAutoConfiguration
+import com.auritylab.spring.gcp.tasks.config.EnableCloudTasks
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.gcp.core.GcpProjectIdProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@EnableCloudTasks
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(classes = [CloudTasksPropertiesTest.TestConfiguration::class])
+@TestPropertySource("/test-base.properties")
+class CloudTasksPropertiesTest {
+    @Configuration
+    class TestConfiguration : CloudTasksLibraryAutoConfiguration() {
+        @Bean
+        fun gcpProjectIdProvider(): GcpProjectIdProvider = GcpProjectIdProvider { "some-project-by-provider" }
+    }
+
+    @Test
+    fun `Test properties for configured values`(
+        @Autowired properties: CloudTasksProperties
+    ) {
+        assert(properties.defaultProjectId == "some-project")
+        assert(properties.defaultLocationId == "europe-west1")
+        assert(properties.defaultQueueId == "some-queue")
+
+        assert(properties.defaultWorkerEndpoint == "http://127.0.0.1:3000")
+        assert(properties.defaultWorkerEndpointRoute == "/taskhandler/test")
+        assert(properties.defaultWorkerRoute == "/test")
+
+        assert(properties.skipCloudTasks)
+        assert(properties.skipTaskEndpoint)
+    }
+}
