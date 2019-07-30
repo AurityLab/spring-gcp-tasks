248d001a4680afadf237b6d7d46b70022ddf0f5d
Andreas Neugebauer <pneugebala@gmail.com>
2019-07-30 16:04:05 +0200

    chore: remove InvalidCloudTasksWorkerEndpointException as it's not needed anymore

--- src/main/kotlin/com/auritylab/spring/gcp/tasks/api/exceptions/InvalidCloudTasksWorkerEndpointException.kt
+++
@@ -1,3 +0,0 @@
-package com.auritylab.spring.gcp.tasks.api.exceptions
-
-class InvalidCloudTasksWorkerEndpointException(message: String, cause: Throwable? = null) : CloudTasksException(message, cause)
