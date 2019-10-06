d23636726822223044bcae21aa8b142ffe0d48c2
Andreas Neugebauer <pneugebala@gmail.com>
2019-10-06 16:39:40 +0200

    fix(config): fix EnableCloudTasksWithEndpoint annotation

--- src/main/kotlin/com/auritylab/spring/gcp/tasks/config/endpoint/EnableCloudTasksWithEndpoint.kt
+++ src/main/kotlin/com/auritylab/spring/gcp/tasks/config/endpoint/EnableCloudTasksWithEndpoint.kt
@@ -1,9 +1,10 @@
 package com.auritylab.spring.gcp.tasks.config.endpoint
 
+import com.auritylab.spring.gcp.tasks.config.EnableCloudTasks
 import org.springframework.context.annotation.Import
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @Import(CloudTasksEndpointConfiguration::class)
 @EnableCloudTasks
-annotation class EnableCloudTasks
+annotation class EnableCloudTasksWithEndpoint
