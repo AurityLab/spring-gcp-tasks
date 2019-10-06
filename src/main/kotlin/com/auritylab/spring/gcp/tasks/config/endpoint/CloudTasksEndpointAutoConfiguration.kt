dd0f1b628d3a47a322f25e1e8e4f9b6d06edd9bc
Andreas Neugebauer <pneugebala@gmail.com>
2019-10-06 17:11:14 +0200

    fix(config): fix some issues with new configurations

--- src/main/kotlin/com/auritylab/spring/gcp/tasks/config/endpoint/CloudTasksEndpointAutoConfiguration.kt
+++ src/main/kotlin/com/auritylab/spring/gcp/tasks/config/endpoint/CloudTasksEndpointAutoConfiguration.kt
@@ -1,13 +1,26 @@
 package com.auritylab.spring.gcp.tasks.config.endpoint
 
+import com.auritylab.spring.gcp.tasks.config.CloudTasksLibraryAutoConfiguration
+import com.auritylab.spring.gcp.tasks.config.EnableCloudTasks
+import com.auritylab.spring.gcp.tasks.core.BeanExplorer
 import com.auritylab.spring.gcp.tasks.core.TaskEndpoint
+import com.auritylab.spring.gcp.tasks.core.signature.TaskSignatureHandler
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 @Configuration
+@EnableCloudTasks
 @Import(
+    CloudTasksLibraryAutoConfiguration::class,
     TaskEndpoint::class
 )
 @ConditionalOnBean(CloudTasksEndpointConfiguration.EndpointMarker::class)
-class CloudTasksEndpointAutoConfiguration
+class CloudTasksEndpointAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean(TaskEndpoint::class)
+    fun taskEndpoint(explorer: BeanExplorer, signatureHandler: TaskSignatureHandler) =
+        TaskEndpoint(explorer, signatureHandler)
+}
--- src/main/kotlin/com/auritylab/spring/gcp/tasks/config/endpoint/EnableCloudTasksWithEndpoint.kt
+++ src/main/kotlin/com/auritylab/spring/gcp/tasks/config/endpoint/EnableCloudTasksWithEndpoint.kt
@@ -1,10 +1,8 @@
 package com.auritylab.spring.gcp.tasks.config.endpoint
 
-import com.auritylab.spring.gcp.tasks.config.EnableCloudTasks
 import org.springframework.context.annotation.Import
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @Import(CloudTasksEndpointConfiguration::class)
-@EnableCloudTasks
 annotation class EnableCloudTasksWithEndpoint
